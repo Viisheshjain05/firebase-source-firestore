e86d94da6a149ffefea37520c12ac8dc7fe79e18
viisheshjain05 <viisheshjain05@gmail.com>
2021-07-03 15:28:01 +0530

    minor fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,6 @@ exports.sourceNodes = async (
 ) => {
   try {
     if (firebase.apps || !firebase.apps.length) {
-
       const cfg = appConfig
         ? appConfig
         : { credential: firebase.credential.cert(credential) };
@@ -25,7 +24,7 @@ exports.sourceNodes = async (
   }
   // Db Setup
   const db = firebase.firestore();
- 
+
   db.settings({
     timestampsInSnapshots: true,
   });
@@ -35,14 +34,12 @@ exports.sourceNodes = async (
 
   // Promise to get data
   const promises = types.map(
-  
     async ({ collection, type, map = (node) => node }) => {
-  
       const snapshot = await db.collection(collection).get();
-  
+
       for (let doc of snapshot.docs) {
         const contentDigest = getDigest(doc.id);
-  
+
         createNode(
           Object.assign({}, map(doc.data()), {
             id: doc.id,
@@ -61,3 +58,70 @@ exports.sourceNodes = async (
   await Promise.all(promises);
   return;
 };
+
+//! Test New Version
+// const firebase = require("firebase-admin")
+// const crypto = require("crypto")
+
+// exports.sourceNodes = (
+//   { boundActionCreators },
+//   { credential, databaseURL, types, quiet = false },
+//   done
+// ) => {
+//   const { createNode } = boundActionCreators
+
+//   firebase.initializeApp({
+//     credential: firebase.credential.cert(credential),
+//     databaseURL: databaseURL
+//   })
+
+//   const db = firebase.database()
+
+//   const start = Date.now()
+
+//   types.forEach(
+//     ({ query = ref => ref, map = node => node, type, path }) => {
+//       if (!quiet) {
+//         console.log(`\n[Firebase Source] Fetching data for ${type}...`)
+//       }
+
+//       query(db.ref(path)).once("value", snapshot => {
+//         if (!quiet) {
+//           console.log(
+//             `\n[Firebase Source] Data for ${type} loaded in`,
+//             Date.now() - start,
+//             "ms"
+//           )
+//         }
+
+//         const val = snapshot.val()
+
+//         Object.keys(val).forEach(key => {
+//           const node = map(Object.assign({}, val[key]))
+
+//           const contentDigest = crypto
+//             .createHash(`md5`)
+//             .update(JSON.stringify(node))
+//             .digest(`hex`)
+
+//           createNode(
+//             Object.assign(node, {
+//               id: key,
+//               parent: "root",
+//               children: [],
+//               internal: {
+//                 type: type,
+//                 contentDigest: contentDigest
+//               }
+//             })
+//           )
+//         })
+//         done()
+//       })
+//     },
+//     error => {
+//       throw new Error(error)
+//     }
+//   )
+// }
+//! Test New Version
